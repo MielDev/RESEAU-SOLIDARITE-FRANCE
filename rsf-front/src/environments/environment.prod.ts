cd8c1004bb600828786c4c835d16a4d80a29b91e
DevMiel <moignon168@gmail.com>
2026-05-11 23:08:35 +0200

    Update environment configuration files

--- rsf-front/src/environments/environment.prod.ts
+++ rsf-front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://api.reseau-solidarite-france.fr/api'
-  apiUrl: 'http://localhost:3229/api'
+  apiUrl: 'https://api.reseau-solidarite-france.fr/api'
+  // apiUrl: 'http://localhost:3229/api'
 };
